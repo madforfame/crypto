f75010eb0069132ee0a6c77f69f63e14b0cccc02
madforfame <drobecqa@f214-07.e-im2ag.ujf-grenoble.fr>
2019-04-08 09:23:21 +0200

    wala

--- cbc.c
+++ cbc.c
@@ -6,23 +6,42 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+char myRandomData[HALF_BLOCK_SIZE*2];
+
 size_t cbc_enc(uint64_t key[2], uint8_t *pt, uint8_t *ct, size_t ptlen){
 	
 	return 0;
 }
 
+void print_tab_1d(char *c, int s){
+	int i;
+	for(i=0;i<s;i++){
+		printf(" %c", *(c+i));
+	}
+}
+
+void rand_data(int f){
+	ssize_t result = read(f, myRandomData, sizeof myRandomData);
+	if (result < 0){
+	    printf("Probleme lors de la crea random");
+	}else{
+		print_tab_1d(myRandomData, HALF_BLOCK_SIZE*2);
+	}
+}
+
 int main(){
 	int rData = open("/dev/urandom", O_RDONLY);
+
 	if(rData >= 0)
 	{
-	    char myRandomData[50];
-	    ssize_t result = read(rData, myRandomData, sizeof myRandomData);
-	    if (result < 0)
-	    {
-	        // something went wrong
-	    }
+	    rand_data(rData);
+
 	}else{
 		printf("Probleme");
 	}
+
+
+
+	close(rData);
 	return 0;
 }
