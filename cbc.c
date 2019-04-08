ea7ffb3966c713136a250297d8a20184a9c22e46
madforfame <drobecqa@f214-07.e-im2ag.ujf-grenoble.fr>
2019-04-08 09:08:32 +0200

    Wala 'est bon+random debut

--- cbc.c
+++ cbc.c
@@ -1,6 +1,10 @@
 #include "tczero.h"
+#include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 size_t cbc_enc(uint64_t key[2], uint8_t *pt, uint8_t *ct, size_t ptlen){
 	
@@ -8,5 +12,17 @@ size_t cbc_enc(uint64_t key[2], uint8_t *pt, uint8_t *ct, size_t ptlen){
 }
 
 int main(){
+	int rData = open("/dev/urandom", O_RDONLY);
+	if(rData >= 0)
+	{
+	    char myRandomData[50];
+	    ssize_t result = read(rData, myRandomData, sizeof myRandomData);
+	    if (result < 0)
+	    {
+	        // something went wrong
+	    }
+	}else{
+		printf("Probleme");
+	}
 	return 0;
 }
